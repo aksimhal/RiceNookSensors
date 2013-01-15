d51d96050dbb26c7a47281c443769d8d83e9d8da
Anish <aks5db@virginia.edu>
2013-01-14 23:01:08 -0500

    GAE Integration
    Added ability to upload data to a local google app engine server

--- DataLoggers/commGAE.py
+++ DataLoggers/commGAE.py
@@ -1,12 +1,13 @@
-#pyserial test
-#08-14-12
+#comm and gae
+#01-14-13
 #Requires PySerial Library 
 
+import urllib
 import serial
 
 ser = serial.Serial()  # create serial port object
 ser.baudrate = 57600
-ser.port = 4 #if it is com port 3, note com port as 2
+ser.port = 21 #if it is com port 3, note com port as 2
 
 ser.open() #open serial port 
 
@@ -14,6 +15,8 @@ print ser.portstr       # check which port was really used
 
 while ser.isOpen():
 	response = ser.read(10)
-	print "trying to read" 
+	#print "trying to read" 
 	print response
+	u = urllib.urlopen('http://localhost:8888/guestbook',data=urllib.urlencode({'id': response}))
+	
 print "done" 
